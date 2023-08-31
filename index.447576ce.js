80b799bce5b9d27868e581bc2ff97ee925f61fb5
kriandr79 <127450901+kriandr79@users.noreply.github.com>
2023-08-31 11:54:29 +0000

    Deploying to gh-pages from @ kriandr79/project-JuiceDev-js@5bf6a7b5831980bd7c7fc12c88ac9b743113014c 🚀

--- index.447576ce.js
+++
@@ -1,2 +0,0 @@
-console.log("I am from header-burger.js"),console.log("I am from categories.js"),console.log("I am from best-sellers.js"),console.log("I am from support-ukraine.js"),console.log("I am from book-modal.js");
-//# sourceMappingURL=index.447576ce.js.map
