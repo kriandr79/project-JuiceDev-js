9ef7149610cbe30423824520f1062a589e7de8bd
777Roma777 <nanoeagle911@gmail.com>
2023-09-02 17:16:28 +0300

    overlay + absent on img, author, title

--- src/js/site-theme.js
+++ src/js/site-theme.js
@@ -1,17 +1,19 @@
-const themeToggle = document.getElementById('themeToggle');
-const body = document.body;
-const allElements = document.querySelectorAll('*');
+// const themeToggle = document.getElementById('themeToggle');
+// const body = document.body;
+// const allElements = document.querySelectorAll('*');
 
-themeToggle.addEventListener('change', () => {
-    if (themeToggle.checked) {
-        body.classList.add('dark-theme');
-        allElements.forEach((element) => {
-            element.classList.add('dark-theme');
-        });
-    } else {
-        body.classList.remove('dark-theme');
-        allElements.forEach((element) => {
-            element.classList.remove('dark-theme');
-        });
-    }
-});
+// themeToggle.addEventListener('change', () => {
+//     if (themeToggle.checked) {
+//         body.classList.add('dark-theme');
+//         allElements.forEach((element) => {
+//             element.classList.add('dark-theme');
+//         });
+//     } else {
+//         body.classList.remove('dark-theme');
+//         allElements.forEach((element) => {
+//             element.classList.remove('dark-theme');
+//         });
+//     }
+// });
+
+// Прибереш потім коменти, бо мені чомусь заважає цей файл. Якщо розкоментити, верстка не працює
