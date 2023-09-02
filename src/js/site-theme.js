193478c599eac500d67d8ff70cef715b3a2a147f
Ostap <Ostapkriven13@gmail.com>
2023-09-02 20:48:35 +0300

    Updaits
    js, scss

--- src/js/site-theme.js
+++ src/js/site-theme.js
@@ -2,16 +2,34 @@ const themeToggle = document.getElementById('themeToggle');
 const body = document.body;
 const allElements = document.querySelectorAll('*');
 
+function addDarkThemeClass(element) {
+    element.classList.add('dark-theme');
+    const children = element.children;
+    for (const child of children) {
+        addDarkThemeClass(child);
+    }
+}
+
+function removeDarkThemeClass(element) {
+    element.classList.remove('dark-theme');
+    const children = element.children;
+    for (const child of children) {
+        removeDarkThemeClass(child);
+    }
+}
+
 function onThemeChange() {
     if (themeToggle.checked) {
-        body.classList.add('dark-theme');
-        allElements.forEach((element) => {
-            element.classList.add('dark-theme');
+        addDarkThemeClass(body);
+        const liElements = document.querySelectorAll('li');
+        liElements.forEach((liElement) => {
+            liElement.classList.add('dark-theme');
         });
     } else {
-        body.classList.remove('dark-theme');
-        allElements.forEach((element) => {
-            element.classList.remove('dark-theme');
+        removeDarkThemeClass(body);
+        const liElements = document.querySelectorAll('li');
+        liElements.forEach((liElement) => {
+            liElement.classList.remove('dark-theme');
         });
     }
 }
