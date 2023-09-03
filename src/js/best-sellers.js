fe9b7180adcb64641042bbd48f2497a6ba0ea759
kriandr79 <127450901+kriandr79@users.noreply.github.com>
2023-09-03 15:38:39 +0300

    fix bestsellers

--- src/js/best-sellers.js
+++ src/js/best-sellers.js
@@ -8,22 +8,7 @@ export async function createMarkup() {
   createGallery(info);
 }
 
-
-
-
-
 const allCategories = document.querySelector('.all-categories');
-
-allCategories.addEventListener('click', function (event) {
-  const target = event.target.closest('.item-link-book');
-
-  // Перевірте, чи клікнуто на елемент з класом 'item-link-book'
-  if (target) {
-    // Отримати атрибут 'list-id' з клікнутого елемента
-    const bookId = target.getAttribute('list-id');
-    console.log('Клікнуто на книжку з ID:', bookId);
-  }
-});
 
 
 function createGallery(data) {
@@ -31,54 +16,57 @@ function createGallery(data) {
   const markup = `
   <h1 class="title-best-sellers">Best Sellers <span class="title-span">Books</span></h1>
     <ul class="list-names list">${data
-    .map(elements => {
-      if (elements.books && elements.books.length > 0) {
-        return `
+      .map(elements => {
+        if (elements.books && elements.books.length > 0) {
+          return `
         <li class="list-categories list">
             <h2 class="list-categories-title">${elements.list_name}</h2>
             <div class="card-container" list-name-id="${elements.list_name}">
             ${elements.books
-            .map(book => {
-              const imgSrc = book.book_image || absentImage; // Використовуємо 'absent-image.png', якщо book.book_image не існує
-              let imgAlt = ''; // Змінна для зберігання значення атрибута alt
-
-              if (book.book_image) {
-                imgAlt = book.title;
-              } else {
-                imgAlt = 'Title Absent';
-              }
-              
-              return `
-                <a class="item-link-book" list-id='${book._id} aria-label="${book.title || 'Title Absent'} by ${book.author || 'Author Absent'}"'>
+              .map(book => {
+                const imgSrc = book.book_image || absentImage; // Використовуємо 'absent-image.png', якщо book.book_image не існує
+                let imgAlt = ''; // Змінна для зберігання значення атрибута alt
+                if (book.book_image) {
+                  imgAlt = book.title;
+                } else {
+                  imgAlt = 'Title Absent';
+                }
+                return `
+                <a class="item-link-book" list-id='${book._id}'>
                   <div class="card-of-book">
                     <img
+                    aria-label="${book.book_image}"
                       src="${imgSrc}"
                       alt="${imgAlt}"
                       class="img-card-title"
                       width="180"
                       height="256"
                       loading="lazy"
-                      
                     />
                     <div class="overlay-book">
                       <p class="overlay-text">quick view</p>
                     </div>
                   </div>
                   <div class="book-description">
-                    <h3 class="card-title">${book.title || 'Title Absent'}</h3>
-                    <p class="card-author">${book.author || 'Author Absent'}</p>
+                    <h3 aria-label="${
+                      book.title || 'Title Absent'
+                    }" class="card-title">${book.title || 'Title Absent'}</h3>
+                    <p aria-label="${
+                      book.author || 'Author Absent'
+                    }" class="card-author">${book.author || 'Author Absent'}</p>
                   </div>
                 </a>`;
-            })
-            .join('')}
+              })
+              .join('')}
             </div>
-        <button class="book-button" type="button" data-id="${elements.list_name
-          }">See More
+        <button class="book-button" type="button" data-id="${
+          elements.list_name
+        }">See More
         </button>
         </li>`;
-      } else {
-        return '';
-      }
+        } else {
+          return '';
+        }
       })
       .join('')}
     </ul>`;
