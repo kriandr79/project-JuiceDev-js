cae255c911b44273b2dc13423e4f2ce553ece459
Viktoriia <127604125+Viktiriia@users.noreply.github.com>
2023-09-04 15:16:12 +0300

    ls

--- src/js/book-modal.js
+++ src/js/book-modal.js
@@ -18,11 +18,11 @@ async function fetchBooks(bookid) {
 const { _id, book_image, list_name, author, description, amazon_product_url, buy_links } = response.data;
 
     const bookCard = `
-      <div class="book-card-modal">
+      <div class="book-card-modal" data-id="${_id}"> 
         <img src="${book_image}" alt="${list_name} Cover" class="book-cover">
-        <div class="book-inform">
-          <h2 class="book-title">${list_name}</h2>
-          <p class="book-author"> ${author}</p>
+        <div class="book-inform"> 
+          <h2 class="book-title" aria-label="${list_name}">${list_name}</h2>
+          <p class="book-author" aria-label="${author}"> ${author}</p>
           <p class="book-description">${description}</p>
           <div ${buy_links}> 
           <a  href="${amazon_product_url}" target="_blank" crossorigin="anonymous" rel="noopener noreferrer nofollow">
@@ -45,48 +45,40 @@ catch (err) {
 console.log(err);
   }}
 
-function onBookClick(e) {
-  
-  const target = e.target.closest('.item-link-book');
+  function onBookClick(e) {
+    const target = e.target.closest('.item-link-book');
     if (target) {
       const bookId = target.getAttribute('list-id');
-      
-      fetchBooks
   
-     
-  fetchBooks(bookId).then((bookCard) => {
+      fetchBooks(bookId).then(bookCard => {
         const instance = basicLightbox.create(`
-          <div class="modal">
-            ${bookCard}
-          </div>`
-        );
+            <div class="modal">
+              ${bookCard}
+            </div>`);
         instance.show();
+  
+        const addBtn = document.querySelector('.js-add');
+        addBtn.addEventListener('click', onhandlerAdd);
       });
     }
   }
   
-
-const PRODUCT_LS_KEY = 'storebook'; 
-
-document.body.addEventListener('click', onhandlerAdd);
-
-function onhandlerAdd(evt) {
-  if (evt.target.classList.contains('js-add')) {
-    const bookId = Number(evt.target.getAttribute('data-id'));
-    const books = JSON.parse(localStorage.getItem(PRODUCT_LS_KEY)) || [];
-    const currentBook = books.find(({ id }) => id === bookId);
-
-    if (currentBook) {
-      currentBook.qty += 1;
-    } else {
-
-      const newBook = { id: bookId, qty: 1 };
-      books.push(newBook);
-    }
-
-    localStorage.setItem(PRODUCT_LS_KEY, JSON.stringify(books));
-  }
+  const PRODUCT_LS_KEY = 'storebook';
   
-
-}
-
+  
+  function onhandlerAdd(evt) {
+      
+      const bookId = evt.target.getAttribute('data-id');
+      console.log('click:', bookId);
+      const books = JSON.parse(localStorage.getItem(PRODUCT_LS_KEY)) || [];
+      const currentBook = books.find(({ id }) => id === bookId);
+  
+      if (currentBook) {
+        currentBook.qty += 1;
+      } else {
+        const newBook = { id: bookId, qty: 1 };
+        books.push(newBook);
+      }
+  
+      localStorage.setItem(PRODUCT_LS_KEY, JSON.stringify(books));
+    }
