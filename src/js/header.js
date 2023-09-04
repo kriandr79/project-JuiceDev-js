de4ea0ba92178008302576aa0f955594dd2eddc2
kriandr79 <127450901+kriandr79@users.noreply.github.com>
2023-09-04 10:46:30 +0300

    fix header nav

--- src/js/header.js
+++ src/js/header.js
@@ -2,6 +2,10 @@ const location = window.location.pathname.slice(1).split('.');
 const locationPageName = location[0];
 const HEADER_NAV_CLASS = 'current'
 
+console.log('Location:', location);
+console.log('PageName:', locationPageName);
+
+
 const headerNavList = document.querySelector('.header-nav-list');
 
 if (locationPageName === 'index') {
