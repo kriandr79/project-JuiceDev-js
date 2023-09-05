e9b6b7db2e4f9e6f39c50ff9e0f77d63d8857e3c
eddvornichenko <127417581+eddvornichenko@users.noreply.github.com>
2023-09-05 21:35:25 +0300

    styles

--- src/js/shopping-list.js
+++ src/js/shopping-list.js
@@ -3,6 +3,140 @@ import './support-ukraine';
 import './site-theme';
 import './btntop.js';
 import './header.js';
-import './firebase-btns.js';
-import './firebase-functions.js';
-import './firebase-modals.js';
+import { offLoader, onLoader } from '../js/loader.js';
+
+const KEY_LS = 'storebook';
+let imgEmptyBig = new URL('/src/images/empty-page@2.png', import.meta.url);
+let imgEmpty = new URL('/src/images/empty-page.png', import.meta.url);
+let imgAmazon = new URL('/src/images/amazon.png', import.meta.url);
+let imgAppleBooks = new URL('/src/images/books.png', import.meta.url);
+let imgBookshop = new URL('/src/images/bookshop.png', import.meta.url);
+let imgTrash = new URL('/src/images/trash-03.png', import.meta.url);
+
+const ulMarkupLS = document.querySelector('.shoppingList-books');
+let loadData = localStorage.getItem(KEY_LS);
+let parsedData = loadData ? JSON.parse(loadData) : null;
+
+const markupBookEmpty = `<li><p class="shoppingList-text">
+  This page is empty, add some books and proceed to order.
+</p>
+<a href="./index.html">
+  <picture>
+    <source srcset="${imgEmptyBig} 2x" type="image/png" />
+    <img
+      class="shoppingList-img"
+      src="${imgEmpty}"
+      alt="Book"
+    />
+  </picture>
+</a></li>`;
+
+document.addEventListener('DOMContentLoaded', () => {
+  loadBookLS();
+  deleteBookId();
+  console.log(parsedData);
+});
+
+function loadBookLS() {
+  if (parsedData && parsedData.length > 0) {
+    markupBookContent(parsedData);
+  } else {
+    ulMarkupLS.innerHTML = markupBookEmpty;
+  }
+}
+
+function markupBookContent(parsedData) {
+  ulMarkupLS.innerHTML = '';
+
+  const markupBookLi = parsedData
+    .map(parsedData => {
+      return `<li class="books-shoppingListLi">
+        <img
+          class="books-shoppingList-img"
+          src="${parsedData.info.book_image}"
+          alt=""
+        />
+        <div class="box-shoppingList-content">
+          <div id="cont" class="box-shoppingList">
+            <div>
+              <h2 class="text-shoppingList-title">${parsedData.info.title}</h2>
+              <p class="text-shoppingList-category">${parsedData.info.publisher}</p>
+            </div>
+            <button class="box-shoppingList-trash" id="${parsedData.info.id}">
+              <img
+                class="box-shoppingList-trash-icon"
+                src="${imgTrash}"
+                alt="trash"
+              />       
+            </button>
+          </div>
+          <p class="text-shoppingList-content">
+            ${parsedData.info.description}
+          </p>
+          <div class="box-shoppingList-link">
+            <p class="text-shoppingList-author">${parsedData.info.author}</p>
+            <ul class="box-shoppingList-shop">
+              <li>
+                <a
+                  class="shop-shoppingList-link"
+                  target="_blank"
+                  href="${parsedData.info.buy_links.find(link => link.name === 'Amazon')?.url}"
+                >
+                  <img
+                    class="shop-shoppingList-img1"
+                    src="${imgAmazon}"
+                    alt="Amazon Link"
+                  />       
+                </a>
+              </li>
+              <li>
+                <a
+                  class="shop-shoppingList-link"
+                  target="_blank"
+                  href="${parsedData.info.buy_links.find(link => link.name === 'Apple Books')?.url}"
+                >                          
+                  <img
+                    class="shop-shoppingList-img2"
+                    src="${imgAppleBooks}"
+                    alt="Apple Books Link"
+                  />           
+                </a>
+              </li>
+              <li>
+                <a
+                  class="shop-shoppingList-link"
+                  target="_blank"
+                  href="${parsedData.info.buy_links.find(link => link.name === 'Bookshop')?.url}"
+                >
+                  <img
+                    class="shop-shoppingList-img2"
+                    src="${imgBookshop}"
+                    alt="Bookshop link"
+                  />       
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </li>`;
+    })
+    .join('');
+
+  ulMarkupLS.innerHTML = markupBookLi;
+}
+
+function deleteBookId() {
+  let dots = document.getElementsByClassName('box-shoppingList-trash');
+
+  for (let i = 0; i < dots.length; i+1) {
+    dots[i].addEventListener('click', e => {
+      let keyId = e.target.parentElement.getAttribute('id');
+      let filtered = parsedData.filter(o => o.id !== keyId);
+
+      localStorage.setItem(KEY_LS, JSON.stringify(filtered));
+      loadData = localStorage.getItem(KEY_LS);
+      parsedData = JSON.parse(loadData);
+      loadBookLS();
+    });
+  }
+}
